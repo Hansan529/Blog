01a75335202c9a83081f608d297983336f88730a
hansan529 <hansan0529@gmail.com>
2023-04-08 10:02:36 +0900

    feat: 메인배너 색상 변화, Nav 무브먼트 애니메이션

---
+++ script/index.js
@@ -0,0 +1,13 @@
+const bannerColor = document.querySelectorAll(".banner .active");
+const handleFixNav = document.querySelector(".fixNav");
+
+setTimeout(() => {
+  bannerColor.forEach((value, index) => {
+    bannerColor[index].style.fontWeight = 700;
+  });
+}, 4000);
+
+setTimeout(() => {
+  handleFixNav.style.transform = "translateX(0)";
+  handleFixNav.style.borderRadius = "0 50px 50px 0";
+}, 4000);
