a587c7fb745ea5b77d21fec11e528031aafb9827
Hansan529 <hansan0529@gmail.com>
2023-04-19 10:34:57 +0000

    update: 환경변수 로딩, 서버 포트 구성

---
+++ src/init.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+
+import "./db";
+import app from "./server";
+
+const PORT = 80;
+
+const handleListening = () => {
+  console.log(
+    `[localhost           ] 🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - ip    ] 🐤 http://192.168.1.57:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - http  ] 🐤 http://hxan.net:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - https ] 🐤 https://hxan.net:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+};
+
+app.listen(PORT, handleListening);
