66e6a8d73007582705885053cea88fd058f19eef
Hansan529 <hansan0529@gmail.com>
2023-04-20 06:19:03 +0000

    docs: 사용자지정 날짜와, 생성날짜 분리

--- src/models/Project.js
+++ src/models/Project.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const projectSchema = new mongoose.Schema({
   url: { type: String, required: true },
   name: { type: String, required: true },
-  date: { type: Date, dafult: Date.now },
+  date: { type: Date, required: true },
   thumbnail: { type: String, required: true },
   languages: [{ type: String, trim: true }],
   description: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 projectSchema.static("formatLanguage", function (languages) {
