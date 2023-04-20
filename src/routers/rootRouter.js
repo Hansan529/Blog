b24fe622a241d352ac0fcabd99ad4484f291c5cb
Hansan529 <hansan0529@gmail.com>
2023-04-20 05:33:12 +0000

    update: 로그아웃, 회원가입 라우터 추가

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,19 @@
 import express from "express";
 
 import { home } from "../controllers/homeControllers";
-import { getLogin, postLogin } from "../controllers/adminControllers";
+import {
+  getJoin,
+  getLogin,
+  logout,
+  postJoin,
+  postLogin,
+} from "../controllers/adminControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/logout", logout);
+rootRouter.route("/join").get(getJoin).post(postJoin);
 
 export default rootRouter;
