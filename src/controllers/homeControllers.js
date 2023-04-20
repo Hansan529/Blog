8a400dd652bf18e88f0aecd29fe9f91f6277bb2c
Hansan529 <hansan0529@gmail.com>
2023-04-20 02:24:29 +0000

    update: 로그인 페이지 렌더링

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,3 +1,7 @@
 export const home = (req, res) => {
   return res.render("home", { pageTitle: "메인 홈페이지" });
 };
+
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "로그인" });
+};
