deba80ff83fadfe74dbac94dd062fd6f0d59f13c
Hansan529 <hansan0529@gmail.com>
2023-04-20 05:29:01 +0000

    fix: 속성값 미기입 수정

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
-  url: { type: String, required },
-  name: { type: String, required },
+  url: { type: String, required: true },
+  name: { type: String, required: true },
   date: { type: Date, dafult: Date.now },
-  thumbnail: { type: String, required },
+  thumbnail: { type: String, required: true },
   languages: [{ type: String, trim: true }],
-  description: { type: String, required },
+  description: { type: String, required: true },
 });
 
 projectSchema.static("formatLanguage", function (languages) {
