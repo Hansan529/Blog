8075ddc8c3f9e3f829581c424812361960cb2c12
Hansan529 <hansan0529@gmail.com>
2023-04-20 03:51:21 +0000

    update: 로그인 요청 추가

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 
-import { getLogin, home } from "../controllers/homeControllers";
+import { getLogin, home, postLogin } from "../controllers/homeControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/login").get(getLogin);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 
 export default rootRouter;
