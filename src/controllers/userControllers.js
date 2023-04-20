59876d75588eec33b74887ed0f0bb72cfb7d21d1
Hansan529 <hansan0529@gmail.com>
2023-04-20 05:54:08 +0000

    update: home 컨트롤러에서 이전작업 완료

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,7 @@
+export const home = (req, res) => {
+  return res.render("home", { pageTitle: "메인 홈페이지" });
+};
+
 export const getJoin = (req, res) => res.render("join");
 
 export const postJoin = async (req, res) => {
