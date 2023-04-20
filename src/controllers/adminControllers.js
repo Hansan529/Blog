a75c8c7cbd41407bc0f0bbb5bfabcda90fa5c8ac
Hansan529 <hansan0529@gmail.com>
2023-04-20 06:17:56 +0000

    docs: 유저 관련 모듈 삭제, 프로젝트 업로드 기능 추가

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,49 +1,30 @@
 import Project from "../models/Project";
-import User from "../models/User";
-import bcrypt from "bcrypt";
-
-export const getJoin = (req, res) => res.render("join");
-
-export const postJoin = async (req, res) => {
-  const {
-    body: { id, password, email, username },
-  } = req;
-  try {
-    await User.create({
-      id,
-      password,
-      email,
-      username,
-    });
-  } catch (err) {
-    return res.send(err);
-  }
-  return res.redirect("/");
-};
-
-export const getLogin = (req, res) => {
-  return res.render("login", { pageTitle: "로그인" });
-};
-
-export const postLogin = async (req, res) => {
-  const {
-    body: { id, password, email, username, keepLogin },
-  } = req;
-  const user = await User.findOne({ id });
-  const pwdCheck = user && (await bcrypt.compare(password, user.password));
-  if (!pwdCheck) {
-    return res.send("에러");
-  }
-  req.session.loggedIn = true;
-  req.session.user = user;
-  return res.redirect("/");
-};
-
-export const logout = (req, res) => {
-  req.session.destroy();
-  return res.redirect("/");
-};
 
 export const getUploadProject = (req, res) => {
-  return res.render("upload");
+  const { errorMessage } = req.session;
+  delete req.session.errorMessage;
+  return res.render("upload", { errorMessage });
+};
+
+export const postUploadProject = async (req, res) => {
+  const {
+    body: { url, name, date, thumbnail, languages, description },
+  } = req;
+
+  console.log("req.body: ", req.body);
+  try {
+    await Project.create({
+      url,
+      name,
+      date,
+      thumbnail,
+      languages: Project.formatLanguage(languages),
+      description,
+    });
+  } catch (error) {
+    req.session.errorMessage =
+      "조건에 맞지 않은 정보를 입력하여 등록을 실패했습니다.";
+    return res.redirect("project");
+  }
+  return res.redirect("/");
 };
