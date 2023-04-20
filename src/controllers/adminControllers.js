727b4ac8da7577d96f41b994d8e53924d1594fd1
Hansan529 <hansan0529@gmail.com>
2023-04-20 05:36:20 +0000

    feat: 회원가입, 로그인 유저생성 및 체크 기능, 로그아웃 추가

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,11 +1,46 @@
+import Project from "../models/Project";
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+export const getJoin = (req, res) => res.render("join");
+
+export const postJoin = async (req, res) => {
+  const {
+    body: { id, password, email, username },
+  } = req;
+  try {
+    await User.create({
+      id,
+      password,
+      email,
+      username,
+    });
+  } catch (err) {
+    return res.send(err);
+  }
+  return res.redirect("/");
+};
+
 export const getLogin = (req, res) => {
   return res.render("login", { pageTitle: "로그인" });
 };
 
-export const postLogin = (req, res) => {
+export const postLogin = async (req, res) => {
   const {
     body: { id, password, email, username, keepLogin },
   } = req;
+  const user = await User.findOne({ id });
+  const pwdCheck = user && (await bcrypt.compare(password, user.password));
+  if (!pwdCheck) {
+    return res.send("에러");
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
+
+export const logout = (req, res) => {
+  req.session.destroy();
   return res.redirect("/");
 };
 
