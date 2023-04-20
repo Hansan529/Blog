4bd03c0b79d45f086323e88bf0be85480a9dc268
Hansan529 <hansan0529@gmail.com>
2023-04-20 03:52:02 +0000

    update: 로컬에 세션 유저 정보 전달

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "hxn Web Developer Blog";
   res.locals.user = req.session.user;
+  res.locals.loggedInUser = req.session.loggedIn;
   next();
 };
