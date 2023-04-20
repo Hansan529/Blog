55edd432e9660552b9354bcbf27f182c88dfe4e4
Hansan529 <hansan0529@gmail.com>
2023-04-20 03:57:22 +0000

    🚚: homeControllers -> adminControllers

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,7 +1,3 @@
 export const home = (req, res) => {
   return res.render("home", { pageTitle: "메인 홈페이지" });
 };
-
-export const getLogin = (req, res) => {
-  return res.render("login", { pageTitle: "로그인" });
-};
