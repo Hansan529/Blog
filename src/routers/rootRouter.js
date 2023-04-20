476b0bd3dcda8cb9519da856a650595b4fd8c6f4
Hansan529 <hansan0529@gmail.com>
2023-04-20 02:24:09 +0000

    update: 로그인 페이지 라우터 추가

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { home } from "../controllers/homeControllers";
+import { getLogin, home } from "../controllers/homeControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
+rootRouter.route("/login").get(getLogin);
 
 export default rootRouter;
