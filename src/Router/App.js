5e4417c93951bafccee1cac5d4fbb371e2238f7b
Hansan529 <hansan0529@gmail.com>
2023-06-16 10:23:19 +0000

    docs Router: 라우터 추가 및 저장 경로 수정

--- src/Router/App.js
+++ src/Router/App.js
@@ -1,11 +1,16 @@
-import Home from "./Home";
+import Home from "../Home";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
+import Login from "../view/screen/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
