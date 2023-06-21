eb55364f24062f1744ff7df7ec0cc262501a1988
hansan529 <hansan0529@gmail.com>
2023-06-21 14:58:23 +0900

    Server server: CORS 설정, 서버에서 JSON으로 받기

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+import apiRouter from "./router/apiRouter";
+
 const express = require("express");
 const morgan = require("morgan");
 const session = require("express-session");
 const flash = require("express-flash");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
@@ -19,8 +22,11 @@ app.use(logger);
 //   })
 // );
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 app.use(flash());
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
+app.use("/api", apiRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
