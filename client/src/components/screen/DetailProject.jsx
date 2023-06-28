98b62b3b855eb05d196e57d5bb7020dcdcc8ed75
hansan529 <hansan0529@gmail.com>
2023-06-28 16:45:12 +0900

    feat: 프로젝트 생성 후, 이동될 디테일 컴포넌트 생성

--- client/src/components/screen/DetailProject.jsx
+++ client/src/components/screen/DetailProject.jsx
@@ -1,10 +1,19 @@
+// Components
+import Header from '../partials/Header';
+import Footer from '../partials/Footer';
+
+// Function
 import { useParams } from 'react-router-dom';
+
+// Package
 
 function DetailProject() {
   const { id } = useParams();
   return (
     <>
+      <Header />
       <img src={`${process.env.REACT_APP_SERVER}/image/${id}`} alt="" />
+      <Footer />
     </>
   );
 }
