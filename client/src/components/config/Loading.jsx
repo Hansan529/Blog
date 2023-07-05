ca2a8a3c54414881602d527f109fb205804f55fa
hansan529 <hansan0529@gmail.com>
2023-07-05 16:23:03 +0900

    feat: 헤더, 푸터 추가

--- client/src/components/config/Loading.jsx
+++ client/src/components/config/Loading.jsx
@@ -1,14 +1,20 @@
 import styles from '../../styles/config/css/Loading.module.css';
+import Footer from '../partials/Footer';
+import Header from '../partials/Header';
 
 function Loading() {
   return (
-    <div className={styles.background}>
-      <p className={styles.Loading}>로딩 중입니다...</p>
-      <img
-        src={`${process.env.PUBLIC_URL}/images/ico/loading.svg`}
-        alt="로딩중"
-      />
-    </div>
+    <>
+      <Header />
+      <div className={styles.background}>
+        <p className={styles.Loading}>로딩 중입니다...</p>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/ico/loading.svg`}
+          alt="로딩중"
+        />
+      </div>
+      <Footer />
+    </>
   );
 }
 
