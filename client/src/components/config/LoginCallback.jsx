6266ccd2faec881f416324531059f296d343e898
hansan529 <hansan0529@gmail.com>
2023-07-06 15:32:09 +0900

    docs: Loading 컴포넌트에 Header, Footer 제거

--- client/src/components/config/LoginCallback.jsx
+++ client/src/components/config/LoginCallback.jsx
@@ -1,18 +1,28 @@
-import { useDispatch } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
+// Component
+import Header from '../partials/Header';
+import Footer from '../partials/Footer';
+import Loading from './Loading';
+
+// Function
 import { server } from '../screen/Home';
-import { useEffect } from 'react';
 import { check } from '../../_redux/_reducer/loginSlice';
 import { info } from '../../_redux/_reducer/InfoSlice';
-import Loading from './Loading';
+
+// Package
+import { useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function LoginCallback() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  // 현재 URL 정보 가져오기
   const location = useLocation();
+  // 경로 생성 #1
   const params = new URLSearchParams(location.search);
   const obj = {};
 
+  // 경로 생성 #2
   params.forEach((value, key) => {
     obj[key] = value;
   });
@@ -20,9 +30,11 @@ function LoginCallback() {
   const data = async () => {
     const data = await (await server.post('/login/github/access', obj)).data;
     if (data.logged) {
+      // 소셜 로그인일 경우, 로그인 처리
       dispatch(check(data.logged));
       navigate('/');
     } else {
+      // 소셜 로그인이 아닐 경우, 데이터를 갖고 회원가입 페이지로 이동
       dispatch(info(data));
       navigate('/join');
     }
@@ -32,7 +44,13 @@ function LoginCallback() {
     data();
   }, [obj]);
 
-  return <Loading />;
+  return (
+    <>
+      <Header />
+      <Loading />
+      <Footer />
+    </>
+  );
 }
 
 export default LoginCallback;
