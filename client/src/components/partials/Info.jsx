4a059529be16bdfad97cf80030ea60a99b8e497d
hansan529 <hansan0529@gmail.com>
2023-07-24 21:10:22 +0900

    feat (info): 정보 컴포넌트 구현

--- client/src/components/partials/Info.jsx
+++ client/src/components/partials/Info.jsx
@@ -1,7 +1,15 @@
+// Components
+
+// Functions
+import styles from '../../styles/partials/css/Info.module.css';
+
+// Packages
 import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 function Info({ LinkInfo }) {
   const infoRef = useRef();
+  const info = useSelector((state) => state.db.info);
 
   // * infoRef가 선택되면, 해당 Ref를 부모 컴포넌트로 전달
   useEffect(() => {
@@ -13,9 +21,17 @@ function Info({ LinkInfo }) {
   }, [infoRef]);
 
   return (
-    <article>
+    <article className={styles.article}>
       <div ref={infoRef} id="info" className="part"></div>
       <h2>Info</h2>
+      <div className={styles.infoWrap}>
+        {info.map((item, index) => (
+          <div className={styles.info} key={index}>
+            <h3>{item.title}</h3>
+            <small>{item.date.slice(0, 10)}</small>
+          </div>
+        ))}
+      </div>
     </article>
   );
 }
