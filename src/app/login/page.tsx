cd708ff3e02a74d268103fcf386cb3b0fb897dba
hansan529 <hansan0529@gmail.com>
2023-08-05 20:29:02 +0900

    docs: 동적인 요소 제거(서버컴포넌트)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,13 +11,6 @@ export default function Login() {
       <div className="formWrap">
         <h2>관리자 로그인</h2>
         <LoginClient />
-        {/* <button
-          className={styles.githubLogin}
-          type="button"
-          onClick={onClickGithub}
-        >
-          깃허브 로그인 &rarr;
-        </button> */}
       </div>
       {/* {error ? (
         <div className={errorStyles.error}>
