0f3163f32cd348ff060692dcc37448feb50c9d68
hansan529 <hansan0529@gmail.com>
2023-08-07 11:00:40 +0900

    feat: 깃허브 소셜로그인

--- src/app/login/LoginClient.tsx
+++ src/app/login/LoginClient.tsx
@@ -6,14 +6,15 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
 import { logIn } from '../redux/feature/auth-slice';
+import api from '../../axios';
 
 type ChangInput = React.ChangeEvent<HTMLInputElement>;
 
 export default function LoginClient() {
   const router = useRouter();
   const dispatch = useDispatch();
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+  const [id, setId] = useState<string>('');
+  const [pw, setPw] = useState<string>('');
   const onChange = (e: ChangInput) => {
     const { name, value } = e.target;
     switch (name) {
@@ -31,24 +32,22 @@ export default function LoginClient() {
     const formData = new FormData();
     formData.append('id', id);
     formData.append('pw', pw);
-    const { data } = await axios.post(
-      `${process.env.NEXT_PUBLIC_API}/user/login`,
-      formData,
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
+    const { data } = await api.post(`/user/login`, formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
     if (!data) {
       return;
     }
     dispatch(logIn(id));
     router.push('/');
-    // router.push('/');
   };
 
-  const authGithub = () => {};
+  const authGithub = async () => {
+    const { data } = await api.get('/user/oauth');
+    router.push(data);
+  };
   return (
     <>
       <form method="POST" onSubmit={onSubmit} className={styles.form}>
