4e8edb8c279f3f3a7480e6c09dbeb93d974a4732
hansan529 <hansan0529@gmail.com>
2023-08-07 23:29:47 +0900

    docs: DB 접속 제어, 중복 연결하지 않도록 수정

--- src/app/api/db/route.ts
+++ src/app/api/db/route.ts
@@ -3,11 +3,12 @@ import { NextResponse } from 'next/server';
 import mongoose from 'mongoose';
 
 export async function GET(req: Request) {
-  mongoose.connect(process.env.DB_URL);
   const db = mongoose.connection;
   let result: string = 'disconnected';
   if (db.readyState >= 1) {
     result = 'connected';
+  } else {
+    mongoose.connect(process.env.DB_URL);
   }
   return NextResponse.json(result);
 }
