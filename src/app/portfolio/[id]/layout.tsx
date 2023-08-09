a2450776f41c499805f40bf366dcb13a7c7b9714
hansan529 <hansan0529@gmail.com>
2023-08-09 15:22:51 +0900

    docs: 스타일시트 제거, inenrStyle로 변경

--- src/app/portfolio/[id]/layout.tsx
+++ src/app/portfolio/[id]/layout.tsx
@@ -1,7 +1,12 @@
-import './layout.scss';
-
 const PortfolioLayout = ({ children }: { children: React.ReactNode }) => {
-  return <section className="portfolio-layout">{children}</section>;
+  return (
+    <section
+      className="portfolio-layout"
+      style={{ maxWidth: '1200px', margin: '0 auto' }}
+    >
+      {children}
+    </section>
+  );
 };
 
 export default PortfolioLayout;
