866e74a654abbd86b464a674b6b4ebe83673ce1f
hansan529 <hansan0529@gmail.com>
2023-08-09 18:58:43 +0900

    docs: 로딩 페이지는 global css의 padding을 받지 않도록 수정

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,13 +1,20 @@
 import styles from './Loading.module.scss';
+import Image from 'next/image';
 
 const Loading = () => {
   return (
-    <>
+    <div className={styles.loadingWrap}>
       <div className={styles.background}>
-        <p className={styles.Loading}>로딩 중입니다...</p>
-        <img src="/loading.svg" alt="로딩중" />
+        <p className={styles.loading}>로딩 중입니다...</p>
+        <Image
+          className={styles.loading_img}
+          src="/images/loading.svg"
+          alt="로딩중"
+          width={200}
+          height={200}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
