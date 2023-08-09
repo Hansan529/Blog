b64288157e7c7b5615acb655d53bd04108d4dd0a
hansan529 <hansan0529@gmail.com>
2023-08-09 11:15:33 +0900

    docs: mongoose 설정

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
   experimental: {
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose'],
     serverActions: true,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+    };
+    return config;
   },
 };
