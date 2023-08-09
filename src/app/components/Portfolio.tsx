6f0eab5a58b441ed6d081d17b5b6c97b9422b178
hansan529 <hansan0529@gmail.com>
2023-08-09 15:21:35 +0900

    feat: 랜딩 페이지 포트폴리오 구현

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
 import { api } from '../../axios';
+import Image from 'next/image';
+import styles from './Portfolio.module.scss';
 
 const Portfolio = async () => {
   const {
     data: { portfolio },
   } = await api.get('/portfolio');
   return (
-    <article>
+    <article className={styles.portfolios}>
       {portfolio.map((item) => (
-        <div key={item._id}>
+        <div
+          className={styles.portfolio_wrap}
+          title={item.title}
+          key={item._id}
+        >
           <Link href={`/portfolio/${item._id}`}>
-            {item.developer}
-            <h2>{item.title}</h2>
+            <h3>{item.title}</h3>
             <small>{item.date}</small>
+            <Image
+              className={styles.portfolioImg}
+              src={item.imageUrl}
+              alt={item.title}
+              width={400}
+              height={225}
+            />
           </Link>
         </div>
       ))}
