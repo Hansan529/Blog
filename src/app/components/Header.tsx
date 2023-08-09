77fdc8f66e69fd0f8ac88dfaa9b5e834927e0d31
hansan529 <hansan0529@gmail.com>
2023-08-09 15:22:34 +0900

    docs: Header Nav 버튼 제거

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -15,35 +15,6 @@ const Header = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const loginChk = useSelector((state: RootState) => state.auth.value.isAuth);
-
-  // * 바로가기 버튼을 누르면, 해당 위치로 스크롤이 이동하도록 하는 함수
-  const onClick = (e) => {
-    const { name } = e.target;
-    // switch (name) {
-    //   case 'homepage':
-    //     if (homepage) {
-    //       homepage.current.scrollIntoView({ behavior: 'smooth' });
-    //     } else {
-    //       navigate('/');
-    //       dispatch(linkPart('homepage'));
-    //     }
-    //     return;
-    //   case 'project':
-    //     if (project) {
-    //       project.current.scrollIntoView({ behavior: 'smooth' });
-    //     } else {
-    //       navigate('/');
-    //       dispatch(linkPart('project'));
-    //     }
-    //     return;
-    //   case 'info':
-    //     info.current.scrollIntoView({ behavior: 'smooth' });
-    //     // dispatch(linkPart('info'));
-    //     return;
-    //   default:
-    //     return;
-    // }
-  };
   return (
     <>
       <header className={styles.header}>
@@ -53,21 +24,21 @@ const Header = () => {
             <Link href="/">
               <picture className={styles.picture}>
                 <source
-                  srcSet="/logo.svg"
+                  srcSet="/images/ico/logo.svg"
                   media="(min-width: 1200px)"
                   type="image/svg+xml"
                   width="60"
                   height="60"
                 />
                 <source
-                  srcSet="/logo-icon.svg"
+                  srcSet="/images/ico/logo-icon.svg"
                   media="(max-width: 1200px)"
                   type="image/svg-xml"
                   width="40"
                   height="40"
                 />
                 <img
-                  src="/logo-icon.svg"
+                  src="/images/ico/logo-icon.svg"
                   alt="Hxan Blog Logo"
                   width="40"
                   height="40"
@@ -75,25 +46,6 @@ const Header = () => {
               </picture>
             </Link>
           </h1>
-          <nav>
-            <ul className={styles.ul}>
-              <li>
-                <button name="homepage" onClick={onClick}>
-                  홈페이지 소개
-                </button>
-              </li>
-              <li>
-                <button name="project" onClick={onClick}>
-                  프로젝트
-                </button>
-              </li>
-              <li>
-                <button name="info" onClick={onClick}>
-                  정보 모음
-                </button>
-              </li>
-            </ul>
-          </nav>
           <div className={styles.btn}>
             {loginChk ? (
               <>
