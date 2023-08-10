f9879ae45a25dc729f6e13f1c5884a55db3d4ca7
hansan529 <hansan0529@gmail.com>
2023-08-10 19:34:13 +0900

    docs(config): webpack 컴파일 제거

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  experimental: {
-    esmExternals: 'loose',
-    serverComponentsExternalPackages: ['mongoose'],
-    serverActions: true,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  webpack: (config) => {
-    config.experiments = {
-      topLevelAwait: true,
-    };
-    return config;
+  experimental: {
+    serverActions: true,
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose'],
   },
 };
