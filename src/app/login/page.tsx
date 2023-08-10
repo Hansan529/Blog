9561294a80f0147105ce67a11904aa66e945958b
hansan529 <hansan0529@gmail.com>
2023-08-10 19:27:14 +0900

    docs(Login): 모듈 스타일시트 참조 이름 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,14 @@
 import { useSelector } from 'react-redux';
 import { logIn } from '../redux/feature/auth-slice';
 import { RootState } from '../redux/store';
-import './page.modules.scss';
+import styles from './page.module.scss';
 import LoginClient from './LoginClient';
 
 // Component
 const Login = () => {
   return (
-    <article className="screen">
-      <div className="formWrap">
+    <article className={styles.screen}>
+      <div className={styles.formWrap}>
         <h2>관리자 로그인</h2>
         <LoginClient />
       </div>
