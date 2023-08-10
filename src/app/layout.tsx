0d7c0504885fc491edaabf6c8dd6b27475bcb3ab
hansan529 <hansan0529@gmail.com>
2023-08-10 21:43:03 +0900

    feat(Footer): 푸터 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,23 @@ import Header from './components/Header';
 import './globals.scss';
 import { Metadata } from 'next';
 import { connectMongoDB } from '../utils/database';
+import Footer from './components/Footer';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const metadata: Metadata = {
-  generator: 'Next.js',
-  applicationName: 'Next.js',
-  colorScheme: 'dark',
-  creator: 'Hansan',
   title: 'Hxan FrontEnd Blog',
   description: '프로젝트 및 개발 정보',
   openGraph: {
-    images: '/og-image.png',
+    title: 'Hxan Portfolio',
+    description: '포트폴리오 랜딩페이지',
+    url: 'https://hxan.net',
+    siteName: 'Hxan Developer Blog',
+    images: '/images/og-image.png',
+    locale: 'ko_KR',
+    type: 'website',
   },
 };
 
@@ -24,88 +27,11 @@ export default async function RootLayout({ children }: Props) {
   await connectMongoDB();
   return (
     <html>
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/favicon/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/favicon/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/favicon/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/favicon/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/favicon/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/favicon/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicon/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/favicon/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicon/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
       <body>
         <ReduxProvider>
           <Header />
           <main>{children}</main>
-          <footer></footer>
+          <Footer />
         </ReduxProvider>
       </body>
     </html>
