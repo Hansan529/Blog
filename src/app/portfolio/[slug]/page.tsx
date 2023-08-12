49dc0b5e4cfd7ccb60ba33c34ea07762146fef53
hansan529 <hansan0529@gmail.com>
2023-08-12 16:22:14 +0900

    feat: 카카오톡, 서브웨이, NXweb 추가

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,15 +1,27 @@
 import 카카오톡클론 from './카카오톡클론';
-
-
+import 서브웨이클론 from "./서브웨이클론";
+import NXweb from "./NXweb";
+import { Metadata } from 'next';
 
 interface Params {
   params: { slug: string };
+}
+
+
+export const metadata: Metadata = {
+  title: '포트폴리오',
+  description: '포트폴리오 목록',
 }
 
 export default function Page({ params }: Params) {
   switch (params.slug) {
     case encodeURIComponent('카카오톡클론'):
       return <카카오톡클론 />;
+      case encodeURIComponent('서브웨이클론'):
+      return <서브웨이클론 />;
+      case encodeURIComponent('NXweb'):
+      return <NXweb />;
+    default:
+      return <div>해당 게시글은 없는 게시글입니다.</div>
   }
-  return;
 }
---
+++ src/app/portfolio/[slug]/서브웨이클론.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import 서브웨이클론 from "./서브웨이클론.mdx";
+import Image from "next/image";
+
+export default function Page(){
+    return (
+        <article className="container mx-auto prose dark:prose-invert">
+        <서브웨이클론 />
+        <Link target="_blank" href='https://github.com/Hansan529/tutorial/tree/main/HRD-step02_CSS/CSS_markup/subway'>
+        <Image
+          className="object-contain hover:opacity-75"
+          src="/portfolio-img/subway.png"
+          alt="서브웨이 클론"
+          width={1200}
+          height={800}
+          priority={true}
+        />
+        </Link>
+      </article>
+        )
+    
+}
