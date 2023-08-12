2b8ef31ef50b927cb965b00b3fc10c0345ab647f
hansan529 <hansan0529@gmail.com>
2023-08-12 14:11:50 +0900

    docs: 마이그레이션 될 파일 제거

--- src/app/portfolio/[id]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import LoadingSkeleton from '../../components/Loading';
-
-const Loading = () => {
-  return <LoadingSkeleton />;
-};
-
-export default Loading;
--- src/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API });
-export const formApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-});
