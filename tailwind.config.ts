225fd9905b8b5aef8b48831b50863dfe55bba5fd
hansan529 <hansan0529@gmail.com>
2023-08-13 19:11:16 +0900

    feat: 테일윈드 너비값 사용자 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,21 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      width: {
+        "1/10": "10%",
+        "2/10": "20%",
+        "3/10": "30%",
+        "4/10": "40%",
+        "5/10": "50%",
+        "6/10": "60%",
+        "7/10": "70%",
+        "8/10": "80%",
+        "9/10": "90%",
+      },
+    },
+  },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
