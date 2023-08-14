f3598d7a0b6388556dd03362b0d2f627d6674e8e
hansan529 <hansan0529@gmail.com>
2023-08-14 21:11:19 +0900

    delete: 커밋 잔디밭 제거

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import GitHubCalendar from "react-github-calendar";
-
 export default function Information() {
-  return (
-    <article className="container mx-auto">
-      <GitHubCalendar username="Hansan529" />
-    </article>
-  );
+  return <article className="container mx-auto"></article>;
 }
