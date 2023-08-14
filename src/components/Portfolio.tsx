dffa1031c785e571813864d2d23c6e089ce242c7
hansan529 <hansan0529@gmail.com>
2023-08-14 21:13:16 +0900

    feat: 포트폴리오 게시글 배열화 및 대표언어 추가

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,53 +2,64 @@ import Link from "next/link";
 import Image from "next/image";
 
 interface List {
-  [key: string]: string;
+  [key: string]: string[];
 }
 
 export default function Portfolio() {
   const list: List[] = [
     // { 블로그: "Blog (Dev)" },
-    { "daily-ui": "Daily UI 100 (Reference)" },
-    { "react-weather": "React Weather (Dev)" },
-    { wetube: "Wetube (Dev)" },
-    { "apple-wallet": "Apple Wallet (Reference)" },
-    { "lg-display": "LG Display (Referenece)" },
-    { wwf: "WWF (Clone)" },
-    { hollys: "Hollys (Clone)" },
-    { nxweb: "NXweb (Clone)" },
-    { northface: "NorethFace (Clone)" },
-    { 서브웨이클론: "Subway (Clone)" },
-    { 카카오톡클론: "KokoaTalk (Clone)" },
+    { "daily-ui": ["Daily UI 100 (Reference)", "ts"] },
+    { "react-weather": ["React Weather (Dev)", "react"] },
+    { wetube: ["Wetube (Dev)", "nodejs"] },
+    { "apple-wallet": ["Apple Wallet (Reference)", "js"] },
+    { "lg-display": ["LG Display (Referenece)", "js"] },
+    { wwf: ["WWF (Clone)", "html"] },
+    { hollys: ["Hollys (Clone)", "html"] },
+    { nxweb: ["NXweb (Clone)", "html"] },
+    { northface: ["NorethFace (Clone)", "html"] },
+    { 서브웨이클론: ["Subway (Clone)", "html"] },
+    { 카카오톡클론: ["KokoaTalk (Clone)", "html"] },
   ];
+
   return (
     <article
-      className="w-8/10 xl:gridCenter container mx-auto space-y-[40px] 
+      className="xl:gridCenter container mx-auto w-8/10 space-y-[40px] 
       xl:w-full xl:gap-[30px] xl:space-y-0"
       style={{
         gridTemplateColumns: "repeat(auto-fill, minmax(350px, auto)",
       }}
     >
       {list.map((key, index) => (
-        <>
-          <Link
-            href={`/portfolio/${Object.keys(key)}`}
-            key={index}
-            className="bg-sky-200 dark:bg-gray-700 outline-sky-200 block overflow-hidden rounded-xl shadow-[0_0_10px_1px_#fff] outline outline-offset-4
+        <Link
+          href={`/portfolio/${Object.keys(key)}`}
+          key={index}
+          className="block overflow-hidden rounded-xl bg-sky-200 shadow-[0_0_10px_1px_#fff] outline outline-offset-4 outline-sky-200 dark:bg-gray-700
             dark:outline-white xl:h-full xl:w-full"
-          >
+        >
+          <div className="relative">
+            <div className="absolute left-[20px] top-1/2 -translate-y-1/2">
+              <Image
+                src={`/ico/language/${Object.values(key).flat()[1]}-icon.svg`}
+                alt={Object.values(key).flat()[1]}
+                width={30}
+                height={30}
+              />
+            </div>
             <h2 className="p-[20px] text-center font-semibold">
-              {Object.values(key)}
+              <span className="inline-block w-[75%] overflow-hidden text-ellipsis whitespace-nowrap align-middle">
+                {Object.values(key).flat()[0]}
+              </span>
             </h2>
-            <Image
-              className="h-[200px] w-full rounded-t-xl bg-[#F1F0E8] object-contain dark:bg-white/20"
-              src={`/portfolio-img/${Object.keys(key)}.png`}
-              alt={String(Object.values(key))}
-              width={300}
-              height={200}
-              priority={true}
-            />
-          </Link>
-        </>
+          </div>
+          <Image
+            className="h-[200px] w-full rounded-t-xl bg-[#F1F0E8] object-contain dark:bg-white/20"
+            src={`/portfolio-img/${Object.keys(key)}.png`}
+            alt={String(Object.keys(key)[0])}
+            width={300}
+            height={200}
+            priority={true}
+          />
+        </Link>
       ))}
     </article>
   );
